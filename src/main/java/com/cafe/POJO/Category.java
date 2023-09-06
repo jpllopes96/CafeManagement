245d56adcf8163f509567cb03be42465bde28a7f
jpllopes96 <joaolacaia96@gmail.com>
2023-09-05 22:24:03 -0300

    5. Cafe Management System - All API of Category

---
+++ src/main/java/com/cafe/POJO/Category.java
@@ -0,0 +1,28 @@
+package com.cafe.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@NamedQuery(name = "Category.getAllCategory", query = "select c from Category c")
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "category")
+
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
