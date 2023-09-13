2c54e8a5b4133ef3dd6fd491649a34ddd288710c
jpllopes96 <joaolacaia96@gmail.com>
2023-09-12 21:42:58 -0300

    API of Bill, Dashboard

--- src/main/java/com/cafe/POJO/Bill.java
+++ src/main/java/com/cafe/POJO/Bill.java
@@ -7,6 +7,8 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import java.io.Serializable;
 
+@NamedQuery(name = "Bill.getAllBills", query = "select b from Bill b order by b.id desc")
+@NamedQuery(name = "Bill.getBillByUserName", query = "select b from Bill b where createdBy=:username order by b.id desc")
 @Data
 @Entity
 @DynamicUpdate
--- src/main/java/com/cafe/restImpl/BillRestImpl.java
+++ src/main/java/com/cafe/restImpl/BillRestImpl.java
@@ -1,5 +1,6 @@
 package com.cafe.restImpl;
 
+import com.cafe.POJO.Bill;
 import com.cafe.constents.CafeConstants;
 import com.cafe.rest.BillRest;
 import com.cafe.service.BillService;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,6 +22,36 @@ public class BillRestImpl implements BillRest {
     public ResponseEntity<String> generateReport(Map<String, Object> requestMap) {
         try {
             return billService.generateReport(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Bill>> getBills() {
+        try{
+            return billService.getBills();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<byte[]> getPdf(Map<String, Object> requestMap) {
+        try{
+            return billService.getPdf(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> deleteBill(Integer id) {
+        try{
+            return billService.deleteBill(id);
         }catch (Exception ex){
             ex.printStackTrace();
         }
